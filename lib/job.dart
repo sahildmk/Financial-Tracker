ad34530229778235ba05490a1e25a01e93eda3dd
sahildmk <z5255280@ad.unsw.edu.au>
2020-07-04 23:47:07 +1000

    Job class

---
+++ lib/job.dart
@@ -0,0 +1,32 @@
+import 'jobCard.dart';
+
+class Job {
+  String name;
+  double rateOfPay;
+  String payFreq;
+  JobCard jobCard;
+
+  Job(String _name, double _rateOfPay, String _payFreq) {
+    this.name = _name;
+    this.rateOfPay = _rateOfPay;
+    this.payFreq = _payFreq;
+    this.jobCard = new JobCard(this.name, "None", "None");
+  }
+
+  String getName() {
+    return this.name;
+  }
+
+  double getRateOfPay() {
+    return this.rateOfPay;
+  }
+
+  String getpayFreq() {
+    return this.payFreq;
+  }
+
+  JobCard getJobCard() {
+    return this.jobCard;
+  }
+
+}
