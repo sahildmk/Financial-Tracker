cb470ff7793047c762ee7cbb2faa59116207dd47
sahildmk <z5255280@ad.unsw.edu.au>
2020-07-04 23:46:58 +1000

    Dialogs file

---
+++ lib/alertDialogs.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+Future<void> deleteDialog(BuildContext context, int index, String item, Function delFunc, Function cancelFunc) async {
+  return showDialog<void>(
+    context: context,
+    barrierDismissible: false, // user must tap button!
+    builder: (BuildContext context) {
+      return AlertDialog(
+        title: Text(
+          'Delete $item',
+          style: GoogleFonts.hammersmithOne(
+            color: Colors.black,
+            fontSize: 17,
+            fontWeight: FontWeight.bold
+          ),
+        ),
+        content: SingleChildScrollView(
+          child: ListBody(
+            children: <Widget>[
+              Text(
+                'Are you sure you want to delete this $item? You will lose all other information related to it.',
+                style: GoogleFonts.hammersmithOne(
+                  color: Colors.black,
+                  fontSize: 17,
+                ),
+              ),
+            ],
+          ),
+        ),
+        actions: <Widget>[
+          FlatButton(
+            child: Text(
+              'Confirm',
+              style: GoogleFonts.hammersmithOne(
+                color: Colors.red,
+                fontSize: 17,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            onPressed: () {
+              delFunc(index, context);
+            },
+          ),
+          FlatButton(
+            child: Text(
+              'Cancel',
+              style: GoogleFonts.hammersmithOne(
+                color: Colors.black,
+                fontSize: 17,
+                fontWeight: FontWeight.bold
+              ),
+            ),
+            onPressed: () {
+              cancelFunc(context);
+            },
+          ),
+        ],
+      );
+    },
+  );
+}
