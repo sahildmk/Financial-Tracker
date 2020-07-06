4bbb00ffc4b1984ce402abebc957f41b710d1858
sahildmk <z5255280@ad.unsw.edu.au>
2020-07-06 23:29:56 +1000

    Job screen

---
+++ lib/JobScreen.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'CustomWidgets.dart';
+
+class JobScreen extends StatefulWidget {
+  final String name;
+  JobScreen(this.name);
+  @override
+  _JobScreenState createState() => _JobScreenState(name);
+}
+
+class _JobScreenState extends State<JobScreen> {
+  String name;
+  _JobScreenState(this.name);
+  @override
+  Widget build(BuildContext context) {
+    double phoneHeight = MediaQuery.of(context).size.height;
+    double phoneWidth = MediaQuery.of(context).size.width;
+    double appBarSize = phoneHeight / 10;
+    return Scaffold(
+      appBar: MyAppBar(phoneWidth, phoneHeight, appBarSize, name),
+      body: Scaffold(
+    
+      ),
+      bottomNavigationBar: myBottomNavBar(),
+    );
+  }
+}
