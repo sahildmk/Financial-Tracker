9063ec5f8553db2f2e7d0b088b38ca9c7d8c4396
sahildmk <z5255280@ad.unsw.edu.au>
2020-07-09 01:19:18 +1000

    Hive saving functionality

--- lib/main.dart
+++ lib/main.dart
@@ -4,15 +4,54 @@ import 'package:shift_tracker/CustomWidgets.dart';
 import 'package:shift_tracker/alertDialogs.dart';
 import 'jobCard.dart';
 import 'newJobForm.dart';
+import 'package:hive/hive.dart';
+import 'package:path_provider/path_provider.dart' as path_provider;
+import 'job.dart';
 
-void main() => runApp(
-  MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: Container(
-      child: HomePage(),
-    ),
-  )
-);
+void main() async{ 
+  WidgetsFlutterBinding.ensureInitialized();
+  final appDirectory = await path_provider.getApplicationDocumentsDirectory();
+  Hive.init(appDirectory.path);
+  Hive.registerAdapter(JobAdapter());
+  Hive.registerAdapter(JobCardAdapter());
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Container(
+        child: FutureBuilder(
+          future: Hive.openBox('jobs'),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            if (snapshot.connectionState == ConnectionState.done) {
+              if (snapshot.hasError) {
+                print("Snapshot Error");
+                print(snapshot.error.toString());
+              } else {
+                return HomePage();
+              }
+            }
+            return Scaffold();
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    Hive.close();
+    super.dispose();
+  }
+}
 
 class HomePage extends StatefulWidget {
   @override
@@ -20,33 +59,54 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<JobCard> jobs = [];
-
   addJob() async {
-    final newJob = await Navigator.push(
-      context, 
-      CupertinoPageRoute(builder: (context) => newJobForm())
-    );
+    setState(() {
+      Navigator.push(
+        context, 
+        CupertinoPageRoute(builder: (context) => newJobForm())
+      );
+    });
     // print(newJob);
-    if (newJob != null) {
-      setState(() {
-        jobs.add(newJob.getJobCard());
-      });
-    }
+    // if (newJob != null) {
+    //   setState(() {
+    //     jobs.add(newJob.getJobCard());
+    //   });
+    // }
   }
 
   removeJob(int index, BuildContext context) {
+    final jobsBox = Hive.box('jobs');
     setState(() {
-      jobs.removeAt(index); 
+      jobsBox.deleteAt(index);
       Navigator.of(context).pop();
     });
-    print(jobs.length);
+    print(jobsBox.length);
   }
 
   cancel(BuildContext context) {
     setState(() { 
       Navigator.of(context).pop();
     });
+  }
+
+  ListView _buildListView(double phoneHeight) {
+    final jobsBox = Hive.box('jobs');
+    return ListView.separated(
+      padding: const EdgeInsets.all(8),
+      itemCount: jobsBox.length,
+      itemBuilder: (BuildContext context, int index) {
+        final currJob = jobsBox.getAt(index) as Job;
+        final currJobCard = currJob.getJobCard();
+        return Dismissible(
+          key: UniqueKey(),
+          onDismissed: (direction) {
+            deleteDialog(context, index, "job", removeJob, cancel);
+          },
+          child: currJobCard
+        );
+      },
+      separatorBuilder: (BuildContext context, int index) => Container(height: phoneHeight / 100)
+    );
   }
 
   @override
@@ -62,20 +122,7 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       appBar: MyAppBar(phoneWidth, phoneHeight, appBarSize, "Jobs"),
-      body: ListView.separated(
-        padding: const EdgeInsets.all(8),
-        itemCount: jobs.length,
-        itemBuilder: (BuildContext context, int index) {
-          return Dismissible(
-            key: UniqueKey(),
-            onDismissed: (direction) {
-              deleteDialog(context, index, "job", removeJob, cancel);
-            },
-            child: jobs[index],
-          );
-        },
-        separatorBuilder: (BuildContext context, int index) => Container(height: phoneHeight / 100)
-      ),
+      body: _buildListView(phoneHeight),
       bottomNavigationBar: myBottomNavBar(),
       floatingActionButton: FloatingActionButton( 
         onPressed: () {
@@ -88,4 +135,6 @@ class _HomePageState extends State<HomePage> {
       ),
     );
   }
+
+  
 }
