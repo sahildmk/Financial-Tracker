08c8b6d9f78e5bab0b28f17b5c3e4184f7ac42fb
sahildmk <z5255280@ad.unsw.edu.au>
2020-07-09 22:47:36 +1000

    Shift Card

---
+++ lib/Custom_Widgets/shiftCard.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hive/hive.dart';
+
+TextStyle fontStyle = GoogleFonts.hammersmithOne(
+  color: Colors.black87,
+  fontWeight: FontWeight.normal,
+  fontSize: 16
+);
+
+@HiveType(typeId: 2)
+class ShiftCard extends StatelessWidget{
+  @HiveField(0)
+  final String date;
+  @HiveField(2)
+  final String start;
+  @HiveField(1)
+  final String end;
+
+  final String length;
+
+  ShiftCard(this.date, this.start, this.end, this.length);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Card(
+          shadowColor: Colors.white,
+          color: Colors.white38,
+          child: InkWell(
+            splashColor: Colors.black.withAlpha(30),
+            onTap: () {},
+            child: Container(
+              width: MediaQuery.of(context).size.width - 20,
+              height: 100,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    child: Row(
+                      children: <Widget>[
+                        RichText(
+                          text: TextSpan(
+                            text: this.date,
+                            style: GoogleFonts.hammersmithOne(
+                              color: Colors.black87,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30
+                            )
+                          ),
+                        ),
+                      ],  
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    child: Row(
+                      children: <Widget>[
+                        Column(
+                          children: <Widget>[
+                            RichText(
+                              text: TextSpan(
+                                text: "Start: " + this.start,
+                                style: fontStyle
+                              ),
+                            ),
+                          ],
+                        ),
+                        Column(
+                          children: <Widget>[
+                            RichText(
+                              text: TextSpan(
+                                text: "End: " + this.end,
+                                style: fontStyle
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    child: Row(
+                      children: <Widget>[
+                        RichText(
+                          text: TextSpan(
+                            text: "Length: " + this.length,
+                            style: fontStyle
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+}
