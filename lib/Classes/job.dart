5be07fa0cf382aadcdd4bdfa15cf6fd1fe1cb0f0
Sahil Deshmukh <sahildmk@gmail.com>
2020-09-04 13:02:59 +1000

    removed hive fields added job id

--- lib/Classes/job.dart
+++ lib/Classes/job.dart
@@ -1,18 +1,23 @@
 import '../Custom_Widgets/jobCard.dart';
 
 class Job {
+  int jobID;
   String name;
   double rateOfPay;
   String payFreq;
   JobCard jobCard;
-  // @HiveField(4)
-  // List<Shift> shifts;
 
-  Job(String _name, double _rateOfPay, String _payFreq) {
+  Job(int jobID, String _name, double _rateOfPay, String _payFreq) {
+    this.jobID = jobID;
     this.name = _name;
     this.rateOfPay = _rateOfPay;
     this.payFreq = _payFreq;
-    this.jobCard = new JobCard(this.name, this.rateOfPay.toString(), this.payFreq, this);
+    this.jobCard =
+        new JobCard(this.name, this.rateOfPay.toString(), this.payFreq, this);
+  }
+
+  int getID() {
+    return this.jobID;
   }
 
   String getName() {
@@ -30,5 +35,4 @@ class Job {
   JobCard getJobCard() {
     return this.jobCard;
   }
-
 }
