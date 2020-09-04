b233ef26c7958399c9566e8599e495b03c8112e8
Sahil Deshmukh <sahildmk@gmail.com>
2020-09-04 13:04:53 +1000

    Removed HIVE

--- lib/Pages/JobScreen.dart
+++ lib/Pages/JobScreen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:hive/hive.dart';
 import 'package:shift_tracker/Classes/job.dart';
 import 'package:shift_tracker/Custom_Widgets/alertDialogs.dart';
 import '../Custom_Widgets/CustomWidgets.dart';
@@ -16,12 +15,9 @@ class _JobScreenState extends State<JobScreen> {
   _JobScreenState(this.job);
 
   removeShift(int index, BuildContext context) {
-    final jobsBox = Hive.box('jobs');
     setState(() {
-      jobsBox.deleteAt(index);
       Navigator.of(context).pop();
     });
-    print(jobsBox.length);
   }
 
   cancel(BuildContext context) {
@@ -31,17 +27,16 @@ class _JobScreenState extends State<JobScreen> {
   }
 
   ListView _buildListView(double phoneHeight) {
-    final jobsBox = Hive.box('jobs');
     return ListView.separated(
       padding: const EdgeInsets.all(8),
-      itemCount: jobsBox.length,
+      itemCount: 0,
       itemBuilder: (BuildContext context, int index) {
-        final currJob = jobsBox.getAt(index) as Job;
+        final currJob = null;
         final currJobCard = currJob.getJobCard();
         return Dismissible(
           key: UniqueKey(),
           onDismissed: (direction) {
-            deleteDialog(context, index, "Shift", removeShift, cancel);
+            // deleteDialog(context, index, "Shift", removeShift, cancel);
           },
           child: currJobCard
         );
