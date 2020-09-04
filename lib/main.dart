01a3662a0775bc8abb9b46723714115bdc72325f
Sahil Deshmukh <sahildmk@gmail.com>
2020-09-04 13:02:47 +1000

    remove unused imports

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:shift_tracker/utils/Database.dart';
-import 'Custom_Widgets/jobCard.dart';
-import 'package:hive/hive.dart';
-import 'package:path_provider/path_provider.dart' as path_provider;
-import 'Classes/job.dart';
 import 'Pages/HomeScreen.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // final appDirectory = await path_provider.getApplicationDocumentsDirectory();
-  // Hive.init(appDirectory.path);
-  // Hive.registerAdapter(JobCardAdapter());
   runApp(MyApp());
 }
 
